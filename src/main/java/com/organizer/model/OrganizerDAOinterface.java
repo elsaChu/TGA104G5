d36e70b65cbadd132342e747a6dbcd56c2c17249
52rondo <52rondo@gmail.com>
2022-12-05 19:13:21 +0800

    added DevMergeToBay testing file

---
+++ src/main/java/com/organizer/model/OrganizerDAOinterface.java
@@ -0,0 +1,14 @@
+package com.organizer.model;
+
+import java.util.List;
+
+public interface OrganizerDAOinterface {
+	
+    public void insert(OrganizerVO organizerVO);
+    public void update(OrganizerVO organizerVO);
+    public void delete(Integer empno);
+    public OrganizerVO findByPrimaryKey(Integer organizerNumber);
+    public List<OrganizerVO> selectAll();
+//  public List<organizerVO> getAll(Map<String, String[]> map); 
+	
+}
---
+++ src/main/java/com/organizer/model/OrganizerDAOinterfaceHibernate.java
@@ -0,0 +1,15 @@
+package com.organizer.model;
+
+import java.util.List;
+
+import com.organizer.model.*;
+
+public interface OrganizerDAOinterfaceHibernate {
+	
+		public abstract OrganizerVOHibernate select(Integer organizerNumber);
+		public List<OrganizerVOHibernate> getAll();
+		public abstract OrganizerVOHibernate insert(OrganizerVOHibernate bean); 
+		public abstract OrganizerVOHibernate update(OrganizerVOHibernate bean);
+		public abstract boolean delete(Integer organizerNumber);
+		
+}
