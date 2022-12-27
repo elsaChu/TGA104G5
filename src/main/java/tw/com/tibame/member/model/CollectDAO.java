f029a9d5aa3bf8dc8171a3936d1747ae1e1dc621
52rondo <52rondo@gmail.com>
2022-12-28 04:33:55 +0800

    add to favorite feature.

--- src/main/java/tw/com/tibame/member/model/CollectDAO.java
+++ src/main/java/tw/com/tibame/member/model/CollectDAO.java
@@ -14,10 +14,39 @@ import tw.com.tibame.util.common.Common;
 
 public class CollectDAO implements CollectDAOinterface{
 
+	private static final String INSERT_STMT = "INSERT INTO COLLECT (number, eventNumber) VALUES (?, ?)";
 	@Override
-	public boolean insert(int memberId, int eventId) {
-		
-		return false;
+	public void insert(int memberId, int eventId) {
+		System.out.println("collect dao used insert one");
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			Class.forName(Common.driver);
+			con = DriverManager.getConnection(Common.URL, Common.USER, Common.PASSWORD);
+			pstmt = con.prepareStatement(INSERT_STMT);
+			pstmt.setInt(1, memberId);
+			pstmt.setInt(2, eventId);
+			pstmt.executeUpdate();
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
 	}
 
 	private static final String GET_ALL_STMT = "SELECT eventNumber FROM COLLECT where number = ?";
--- src/main/resources/static/front-end/main/js/indexMainCopy.js
+++ src/main/resources/static/front-end/main/js/indexMainCopy.js
@@ -2,8 +2,13 @@
 // window on load 
 window.addEventListener("DOMContentLoaded", function () {
    console.log("DOM Content Loaded");
-  let loggedIn = false;
-  checkLogIn();
+  var loggedIn = null;
+//  checkLogIn();
+	checkUserLogin(); 
+	console.log("after checkUserLogin");
+	console.log(loggedIn);
+	loggedIn = $("#memid").val();
+	console.log("after jquery", loggedIn);
   getBannerPic();
   getAllEvents();
   $(document).on("click", ".searchResult", function () {
@@ -15,6 +20,35 @@ window.addEventListener("DOMContentLoaded", function () {
 	  console.log("click favorite");
 	//  $(this).css("border","3px solid black");
   });
+  
+//  clicked on icon inside div.searchResult START (including favorite feature)
+	//click on heart (and its outer a tag)
+    $(document).on("click", "ul.featured__item__pic__hover li a", function (e) {
+		if($(this).find("i.fa-heart")){
+			console.log("have heart");
+			if(loggedIn == ""){
+				console.log("loggedIn empty string");
+			}else if(loggedIn == null){
+				alert("logged in ==null 請先登入");
+			}else{
+				let eventID = $(this).closest("div.searchResult").attr("eventNumber");
+				console.log("clicked heart icon on event ", eventID);
+				console.log("loggedin?: ", loggedIn);
+				e.preventDefault();
+				e.stopPropagation();
+				addToFavorite(eventID);
+			}
+		}else{
+			console.log("clicked icon");
+			e.preventDefault();
+			e.stopPropagation();
+		}
+	});
+
+
+//  clicked on icon inside div.searchResult END
+
+
   $("div.gallery-cell").click(function(e){
     let thisEvent = $(this).attr("eventIs");
     console.log(thisEvent);
@@ -28,6 +62,7 @@ window.addEventListener("DOMContentLoaded", function () {
     $(".notLoggedIn").attr("hidden", true);
   });
 });
+
 
 function checkLogIn() {
   fetch('../../CheckLogin', {
@@ -47,7 +82,19 @@ function checkLogIn() {
         }
     });
 
- };
+ }
+ 
+function checkUserLogin(){
+	console.log("memberid from hidden input", $("#memid").val());
+	if($("#memid").val() == ""){
+		console.log("memid is empty string")
+	}else if ($("#memid").val() == null){
+		console.log("memid is null");
+	}else{
+		loggedIn = $("#memid").val();
+		console.log("typeof: ", typeof loggedIn,": ", loggedIn);
+	}
+}
 
 function getBannerPic() {
   fetch('../../GetBanner', {
@@ -62,9 +109,9 @@ function getBannerPic() {
           // alert(body.successful);
           console.log("get　Banner success");
           let ar1 = eval(body.bannerL);
-          console.log("ar1:",ar1)
+//          console.log("ar1:",ar1)
           ar1.forEach(function(e, i){
-			console.log(e,i);
+//			console.log(e,i);
 			if(i < 5){
 			$("div.gallery-cell").eq(i).find("img").attr("src","../../DBGifReader?eventId='" + e + "'");
 			$("div.gallery-cell").eq(i).attr("eventIs", e )
@@ -80,7 +127,7 @@ function getBannerPic() {
           // alert("successful is not true")
         }
     });
-};
+}
 
 function getAllEvents(){
   //	fetch('http://localhost:8080/TGA104G5/DBGifReader?' + new URLSearchParams({"mainSearch": keyword.value}), {
@@ -101,9 +148,9 @@ function getAllEvents(){
         var eventObj = JSON.parse(allEvents1);
               $("div.featured__filter").html("");
         eventObj.forEach(function(e, index){
-          console.log("index: ", index);
-          console.log("element", e);
-          console.log("element.eventNum: ", e.eventNumber);
+//          console.log("index: ", index);
+//          console.log("element", e);
+//          console.log("element.eventNum: ", e.eventNumber);
           
           //Add New List Into ul Start
            let list_html = "";
@@ -119,9 +166,9 @@ function getAllEvents(){
               + 'alt="" style="width: 100%" />';
              
              list_html +=          `<ul class="featured__item__pic__hover">
-                                        <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-heart"></i></a></li>`;
+//              list_html +=              '<li><a href="#"><i class="fa fa-retweet"></i></a></li>';
+             list_html +=             ` <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
                                     </ul>
                                 </div>
                                 <div class="featured__item__text">`;
@@ -136,6 +183,33 @@ function getAllEvents(){
                 $("div.featured__filter").append(list_html);
           })
         });
-  
+};
+
+function addToFavorite(eventid){
+//	get member id see if member logged in
+//		if not send alert "go log in"
+//	get event id for this gallery cell
+//	send request to servlet with eventid and memberid to add to collect DB 
+//	if servlet already has this then dont add send back message saying already in favorite
+	fetch('../../AddFavorite?' + new URLSearchParams({"heartEvent": eventid}), {
+    method: 'get',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+  })
+    .then(function (resp) { return resp.json(); })
+    .then(function (body) {
+      if(body.addNewFav == "success"){
+          alert("Added to Favorite");
+        }else if (body.addNewFav == "foundSame"){
+          alert("already in favorite");
+        }else if (body.addNewFav == "fail"){
+          console.log("Add to Favorite Failed");
+          // alert("successful is not true")
+        }else{
+			console.log("failed add to favorite");
+		}
+    });
+	
 }
  
