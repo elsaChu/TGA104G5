ff5fe08cef5f4c8a87df98c228945e58579769c3
wei <holalion@gmail.com>
2023-01-01 01:28:52 +0800

    前端頁面修正錯誤

--- src/main/resources/static/front-end/member/js/order.js
+++ src/main/resources/static/front-end/member/js/order.js
@@ -1,18 +1,5 @@
 function init() {
   const order = document.querySelector("#order");
-  // sessionStorage.setItem("URL_before_login", window.location.href);
-  // $.ajax({
-  //     url: "../../order/orderlist",           // 資料請求的網址
-  //     type: "GET",                                                     // GET | POST | PUT | DELETE | PATCH
-  //     // data: { "number": number },                                      // 將物件資料(不用雙引號) 傳送到指定的 url
-  //     dataType: "json",                                                // 預期會接收到回傳資料的格式： json | xml | html
-  //     success: function(data){                                         // request 成功取得回應後執行
-  //       console.log(data);
-  //       order.innerHTML = 
-  //       data.map((e) => Template(e.prodOrderNo, e.amountPrice, e.paymentDate, e.prodOrderStatus, e.deliveryStatus)).join('')
-  //     },
-
-  //   });
 
   fetch(`../../order/orderlist?number=`)
     .then((r) => {
@@ -45,7 +32,7 @@ function Template(prodOrderNo, amountPrice, paymentDate, prodOrderStatus, delive
   return `
     <tr>
     <td># ${prodOrderNo}</td>
-    <td>$ ${amountPrice}</td>
+    <td>$${amountPrice}</td>
     <td>${paymentDate}</td>
     <td>${prodOrderStatus}</td>
     <td>${deliveryStatus}</td>
@@ -58,7 +45,6 @@ function Template(prodOrderNo, amountPrice, paymentDate, prodOrderStatus, delive
 
 function showDetail(prodOrderNo) {
   sessionStorage.setItem('prodOrderNo', prodOrderNo);
-  // location = 'orderdetail.html#tab-4';
   location = './orderdetail.html';
 }
 
